6b68c14b678a91f592287dcb2ee1debb93e9f4c3
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-04-01 13:51:37 +0100

    day-1

---
+++ Day-1/main.py
@@ -0,0 +1,14 @@
+print("""Bakare Muideen Adeleke, 4/1/2023
+I am signing up for Replit's 100 days
+of Python challenge!
+I will make sure to spend some time
+every day coding along, for a minimum
+of 10 minutes a day.
+I'll be using Replit, an amazing
+online IDE so I can do this from my
+phone wherever I happend to be. No
+excuses for not coding from the
+middle of a field!
+I am feeling exicted í¸„
+You can follow my progress at replit.com/@BakareMuideen""")
+
