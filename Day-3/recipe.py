53ac01ff16f8959df6df43d4336e9f1420b4df7e
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-04-02 14:20:12 +0100

    concatenate

---
+++ Day-3/recipe.py
@@ -0,0 +1,32 @@
+print("Starter Recipe")
+print("---------------")
+starter_name = input("What is the name of the starter dish? ")
+starter_ingredients = input("What are the ingredients for the starter dish? ")
+starter_instructions = input("What are the instructions for preparing the starter dish? ")
+
+print("\nMain Course Recipe")
+print("-------------------")
+main_name = input("What is the name of the main course? ")
+main_ingredients = input("What are the ingredients for the main course? ")
+main_instructions = input("What are the instructions for preparing the main course? ")
+
+print("\nDessert Recipe")
+print("----------------")
+dessert_name = input("What is the name of the dessert? ")
+dessert_ingredients = input("What are the ingredients for the dessert? ")
+dessert_instructions = input("What are the instructions for preparing the dessert? ")
+
+print("\nComplete Recipe")
+print("----------------")
+print("Starter: " + starter_name)
+print("Ingredients: " + starter_ingredients)
+print("Instructions: " + starter_instructions)
+
+print("\nMain Course: " + main_name)
+print("Ingredients: " + main_ingredients)
+print("Instructions: " + main_instructions)
+
+print("\nDessert: " + dessert_name)
+print("Ingredients: " + dessert_ingredients)
+print("Instructions: " + dessert_instructions)
+
