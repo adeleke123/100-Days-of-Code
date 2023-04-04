17604d8b15522588fd31a0f17ab2b4ea6cba5399
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-04-04 15:30:39 +0100

    print

--- Day-4/main.py
+++ Day-4/main.py
@@ -1,23 +1,28 @@
 print("==Day 4 challenge==")
 print()
 Name = input("What is your name? ")
+print()
 Like = input("what do you like doing? ")
+print()
 Mindset = input("What do you like in people? ")
+print()
 Color = input("What is your favourite color? ")
+print()
 Food = input("What is your Best food? ")
+print()
 Learn = input("What do you have passion for? ")
 print()
 
-print("Once upon a time, there was a young man named", {Name}, ".", {Name}, "was a", {Like}, "enthusiast who spent most of his days tinkering with new technologies and learning new programming languages. He was always on the lookout for the latest trends in the tech industry and was passionate about building software that could make a difference in people's lives."
+print("Once upon a time, there was a young man named", Name, ".", Name, "was a", Like, "enthusiast who spent most of his days tinkering with new technologies and learning new programming languages. He was always on the lookout for the latest trends in the tech industry and was passionate about building software that could make a difference in people's lives."
 
-"However, there was one thing that", {Name}, "couldn't stand: people with a fixed mindset. He believed that everyone should be open to", {Learn}, "and expanding their horizons.", {Name}, "believed that a", {Mindset}, "was the key to success in life and that people who refused to embrace change were limiting themselves."
+"However, there was one thing that", Name, "couldn't stand: people with a fixed mindset. He believed that everyone should be open to", Learn, "and expanding their horizons.", Name, "believed that a", Mindset, "was the key to success in life and that people who refused to embrace change were limiting themselves."
 
-"Despite his love for technology", {Name}, "had other interests as well. His favorite color was", {Color}, "and he loved to wear", {Color}, "clothing whenever he could. He also enjoyed eating", {Food}, "a local delicacy made from yam and other ingredients.",
+"Despite his love for technology", Name, "had other interests as well. His favorite color was", Color, "and he loved to wear", Color, "clothing whenever he could. He also enjoyed eating", Food, "a local delicacy made from yam and other ingredients.",
 
-{Name}, "was always seeking new experiences and opportunities to learn. He was a voracious reader and consumed books on a wide range of topics, from history and philosophy to science and technology."
+Name, "was always seeking new experiences and opportunities to learn. He was a voracious reader and consumed books on a wide range of topics, from history and philosophy to science and technology."
 
-"One day,", {Name}, "decided to attend a local technology conference to network with other like-minded individuals. At the conference, he met many fascinating people who shared his passion for", {Like}, "and learning.", "However, he also encountered a few people with fixed mindsets who dismissed his ideas and refused to listen to his perspectives.",
+"One day,", Name, "decided to attend a local technology conference to network with other like-minded individuals. At the conference, he met many fascinating people who shared his passion for", Like, "and learning.", "However, he also encountered a few people with fixed mindsets who dismissed his ideas and refused to listen to his perspectives.",
 
-"Despite these setbacks,", {Name}, "remained undeterred. He continued to pursue his passion for technology and learning, refusing to let anyone else dictate his path in life.", "He continued to wear his", {Color}, "clothing proudly and savored every bite of his beloved", {Food},
+"Despite these setbacks,", Name, "remained undeterred. He continued to pursue his passion for technology and learning, refusing to let anyone else dictate his path in life.", "He continued to wear his", Color, "clothing proudly and savored every bite of his beloved", Food,
 
-"In the end", {Name}, "perseverance paid off. He landed a job at a cutting-edge tech company, where he was able to put his skills and passions to work. And he continued to learn and grow, embracing new challenges and opportunities as they came his way", ".For", {Name}, "life was a journey of discovery and growth", "and he was always ready for the next adventure",)
+"In the end", Name, "perseverance paid off. He landed a job at a cutting-edge tech company, where he was able to put his skills and passions to work. And he continued to learn and grow, embracing new challenges and opportunities as they came his way", ".For", Name, "life was a journey of discovery and growth", "and he was always ready for the next adventure",)
